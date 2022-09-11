fca080147f88918af68f68b5f3a628e0fc7e43cf
Godofdes <1015229318@qq.com>
2022-09-12 00:53:36 +0800

    project run

--- online-taxi-public/api-passenger/src/main/java/com/msb/apipassenger/service/VerificationCodeService.java
+++ online-taxi-public/api-passenger/src/main/java/com/msb/apipassenger/service/VerificationCodeService.java
@@ -5,7 +5,10 @@ import com.msb.internalcommon.dto.ResponseResult;
 import com.msb.internalcommon.response.NumberCodeResponse;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class VerificationCodeService {
@@ -13,13 +16,21 @@ public class VerificationCodeService {
     @Autowired
     private ServiceVerificationCodeClient serviceVerificationCodeClient;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    //验证码前缀
+    private String verificationCodePrefix = "passenger-verification-code-";
+
     public String generatorCode(String passengerPhone){
         //调用验证码服务，获取验证码
         ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVerificationCodeClient.getNumberCode(6);
 
-        int NumberCode = numberCodeResponse.getData().getNumberCode();
+        int numberCode = numberCodeResponse.getData().getNumberCode();
 
         //存入redis
+        String key = verificationCodePrefix+passengerPhone;
+        stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.MINUTES);
 
         //返回值
         JSONObject result = new JSONObject();
